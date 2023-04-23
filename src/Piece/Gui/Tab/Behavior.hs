12d0c0ed61d5b619f4165712c8ecaedd99517821
Magnus Møller <magnus108@me.com>
2023-04-23 12:57:28 +0200

    tmp

--- src/Piece/Gui/Tab/Behavior.hs
+++ src/Piece/Gui/Tab/Behavior.hs
@@ -6,12 +6,15 @@ module Piece.Gui.Tab.Behavior
 where
 
 import qualified Graphics.UI.Threepenny.Core as UI
+import Graphics.UI.Threepenny.Elements as UI
 import qualified Piece.App.Env as Env
 import qualified Piece.CakeSlayer.Has as Has
 import qualified Piece.Core.Tab as Tab
 import qualified Piece.Db.Db as Db
+import qualified Piece.Db.ListZipper as LZ
 import qualified Piece.Db.Tab as DbTab
 import qualified Reactive.Threepenny as R
+import qualified Relude.Unsafe as Unsafe
 
 showTab :: (Env.WithTabEnv env m) => m (R.Behavior (Db.DatabaseKey -> String))
 showTab = do
@@ -23,13 +26,14 @@ displayTab = do
   show <- showTab
   return $ (UI.string .) <$> show
 
-bListBox :: (Env.WithTabEnv env m) => UI.Behavior (String -> Bool) -> m (R.Behavior [Db.DatabaseKey])
+bListBox :: (Env.WithTabEnv env m) => UI.Behavior (String -> Bool) -> m (R.Behavior (LZ.ListZipper Db.DatabaseKey))
 bListBox bFilterTab = do
   tabEnv <- Has.grab @Env.TabEnv
   let bDatabaseTab = Env.bDatabaseTab tabEnv
   bShowTab <- showTab
   return $
-    (\p display -> filter (p . display) . Db.keys)
-      <$> bFilterTab
-      <*> bShowTab
-      <*> bDatabaseTab
+    fmap (Unsafe.fromJust . LZ.fromList) $
+      (\p display -> filter (p . display) . Db.keys)
+        <$> bFilterTab
+        <*> bShowTab
+        <*> bDatabaseTab
--- src/Piece/Gui/Tab/Tab.hs
+++ src/Piece/Gui/Tab/Tab.hs
@@ -8,6 +8,7 @@ module Piece.Gui.Tab.Tab
   )
 where
 
+import Control.Comonad
 import qualified Data.Map as Map
 import qualified Graphics.UI.Threepenny.Attributes as UI
 import qualified Graphics.UI.Threepenny.Core as UI
@@ -17,6 +18,7 @@ import qualified Piece.App.Env as Env
 import qualified Piece.App.Monad as Monad
 import qualified Piece.CakeSlayer.Has as Has
 import qualified Piece.Core.Tab as Tab
+import qualified Piece.Db.ListZipper as LZ
 import qualified Piece.Db.ListZipper as ListZipper
 import qualified Piece.Gui.Tab.Behavior as Behavior
 import qualified Reactive.Threepenny as R
@@ -51,48 +53,62 @@ setup env = mdo
   return Create {..}
 
 data ZipperBox a = ZipperBox
-  { _elementLB :: UI.Element
-  -- , _selectionLB :: Tidings (Maybe a)
+  { _elementZB :: UI.Element,
+    _selectionZB :: R.Tidings (LZ.ListZipper a)
   }
 
-instance UI.Widget (ZipperBox a) where getElement = _elementLB
+instance UI.Widget (ZipperBox a) where getElement = _elementZB
 
--- | User changes to the current selection (possibly empty).
--- userSelection :: ListBox a -> Tidings (Maybe a)
--- userSelection = _selectionLB
+userSelection :: (ZipperBox a) -> R.Tidings (LZ.ListZipper a)
+userSelection = _selectionZB
 
--- | Create a 'ListBox'.
 zipperBox ::
-  forall a.
-  Ord a =>
-  UI.Behavior [a] ->
-  -- | display for an item
+  UI.Behavior (LZ.ListZipper a) ->
   UI.Behavior (a -> UI.UI UI.Element) ->
   UI.UI (ZipperBox a)
 zipperBox bitems bdisplay = do
-  list <- UI.select
+  nav <- navBox bdisplay
 
-  -- animate output items
-  UI.element list UI.# UI.sink items (map <$> bdisplay <*> bitems)
+  UI.sink items bitems (return nav)
 
-  -- animate output selection
-  let bindices :: UI.Behavior (Map.Map a Int)
-      bindices = (Map.fromList . flip zip [0 ..]) <$> bitems
-      bindex = undefined -- lookupIndex <$> bindices <*> bsel
-      lookupIndex indices Nothing = Nothing
-      lookupIndex indices (Just sel) = Map.lookup sel indices
+  --  UI.element nav UI.# UI.sink UI.selection bindex
 
-  UI.element list UI.# UI.sink UI.selection bindex
-
-  let bindices2 :: UI.Behavior (Map.Map Int a)
-      bindices2 = Map.fromList . zip [0 ..] <$> bitems
-
-      _selectionLB =
-        UI.tidings undefined undefined -- bsel $ undefined
-        -- lookupIndex <$> bindices2 UI.<@> UI.selectionChange list
-      _elementLB = list
+  let _selectionZB =
+        UI.tidings bitems (_click nav)
+      _elementZB = (UI.getElement nav)
 
   return ZipperBox {..}
 
-items = UI.mkWriteAttr $ \i x -> void $ do
-  return x UI.# UI.set UI.children [] UI.#+ map (\i -> UI.option UI.#+ [i]) i
+data NavBox a = NavBox
+  { _elementNav :: UI.Element,
+    _handler :: R.Handler (LZ.ListZipper a),
+    _click :: R.Event (LZ.ListZipper a),
+    _display :: R.Behavior (a -> UI.UI UI.Element)
+  }
+
+instance UI.Widget (NavBox a) where getElement = _elementNav
+
+navBox :: UI.Behavior (a -> UI.UI UI.Element) -> UI.UI (NavBox a)
+navBox bdisplay = do
+  (eClick, hClick) <- liftIO R.newEvent
+  nav <- UI.div
+  let _elementNav = nav
+  return NavBox {..}
+
+items :: UI.WriteAttr (NavBox a) (LZ.ListZipper a)
+items = items' _handler _display
+
+items' :: (NavBox a -> R.Handler (LZ.ListZipper a)) -> R.Behavior (a -> UI.UI UI.Element) -> UI.WriteAttr (NavBox a) (LZ.ListZipper a)
+items' f d = UI.mkWriteAttr $ \i x -> void $ do
+  UI.element x
+    UI.# UI.set UI.children []
+    UI.#+ LZ.toList
+      ( extend
+          ( \wa -> do
+              button <- UI.button UI.#+ [extract wa]
+              UI.on UI.click button $ \_ -> void $ do
+                liftIO $ f x wa
+              return button
+          )
+          i
+      )
